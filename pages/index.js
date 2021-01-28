7dc079dda4c04136cf65f75c78315ed8d91d9684
Eler <gl13eler@gmail.com>
2021-01-28 01:02:02 -0300

    Primeira aula ok

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 import db from '../db.json'
 import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import GitHubCorner from '../src/components/GitHubCorner'
+import Footer from '../src/components/Footer'
 
 
 // const BackgroundImage = styled.div`
@@ -22,7 +25,7 @@ const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
           <Widget.Header>
@@ -34,14 +37,13 @@ export default function Home() {
         </Widget>
         
         <Widget>
-          <Widget.Header>
-            <h1>Gui</h1>
-          </Widget.Header>
           <Widget.Content>
             <p>xanaia sumbatumba habadaia</p>
           </Widget.Content>
         </Widget>
+        <Footer />
       </QuizContainer>
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/GuiEler" />
+    </QuizBackground>
   )
 }
