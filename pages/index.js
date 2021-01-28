960a8ccda90a0d3d55d1e69df92015f514ffdcad
Eler <gl13eler@gmail.com>
2021-01-28 00:51:31 -0300

    Second commit

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,47 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg}); 
+//   flex: 1;
+//   background-position: center;
+//   background-size: cover;
+// `
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
-// function Title(props) {
-//   return (
-//     <h1>
-//       {props.children}
-//     </h1>
-//   )
-// }
-
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Gui</h1>
+          </Widget.Header>
+          <Widget.Content>    
+            <p>xanaia sumbatumba habadaia</p>
+          </Widget.Content>
+        </Widget>
+        
+        <Widget>
+          <Widget.Header>
+            <h1>Gui</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>xanaia sumbatumba habadaia</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  )
 }
